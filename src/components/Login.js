754410afb81fe595e0bd057786a369615ed39623
Jatin Vaidya <jatinvaidya@xf40mm9g7q.lan>
2022-02-16 11:43:11 +1100

    login route

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-let Login = () => {
-    let { loginWithRedirect } = useAuth0;
-    loginWithRedirect();
+const Login = () => {
+    const { loginWithRedirect } = useAuth0();
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    const organization = params.get('organization');
+    const invitation = params.get('invitation');
+    if(organization && invitation) {
+        loginWithRedirect({organization, invitation});    
+    } else {
+        loginWithRedirect();
+    }
 }
 
 export default Login;
